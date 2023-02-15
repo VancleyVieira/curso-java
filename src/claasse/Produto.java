b9cce438265707a64d3cb9d031c4a53920aa3763
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-02-15 07:35:38 -0300

    implementando METODOS na classe Produto

--- src/claasse/Produto.java
+++ src/claasse/Produto.java
@@ -5,4 +5,18 @@ public class Produto {
     String nome;
     double preco;
     double desconto;
+
+    // Assinatura do metodo = "precoDesconto()"
+    double precoDesconto() {
+        return preco * (1 - desconto);
+    }
+
+    // Mostrando que dois metodos podem ter o mesmo nome
+    // Se os parametros forem diferentes
+    // Assinatura do metodo = "precoDesconto (double descontoDoGerente)"
+    double precoDesconto (double descontoDoGerente) {
+        return preco * (1 - desconto + descontoDoGerente);
+    }
+
+    // metodos podem ter o mesmo nome, mas nunca a mesma assinatura
 }
--- src/claasse/ProdutoTeste.java
+++ src/claasse/ProdutoTeste.java
@@ -14,15 +14,10 @@ public class ProdutoTeste {
         p2.desconto = 0.29;
 
         System.out.println(p1.nome);
-        System.out.println(p1.preco);
-        System.out.println(p1.desconto);
-
         System.out.println(p2.nome);
-        System.out.println(p2.preco);
-        System.out.println(p2.desconto);
 
-        double precoFinal1 = p1.preco * (1 - p1.desconto);
-        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        double precoFinal1 = p1.precoDesconto();
+        double precoFinal2 = p2.precoDesconto(0.1);
         double mediaCarrinho = ((precoFinal1 + precoFinal2) / 2);
 
         System.out.printf("Media do carrinho = R$%.2f.", mediaCarrinho);
