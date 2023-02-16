11456e70807ae10bc1a7fad66eb277d9fb45e26c
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-02-16 07:04:18 -0300

    Desafio implementar contrutores na classe Data

--- src/claasse/desafioData/Data.java
+++ src/claasse/desafioData/Data.java
@@ -6,6 +6,18 @@ public class Data {
     int mes;
     int ano;
 
+    Data() { // construtor padrao
+        this.dia = 1;
+        this.mes = 1;
+        this.ano = 1970;
+    }
+
+    Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
     String obterDataFormatada() {
         return this.dia + "/" + this.mes + "/" + this.ano;
     }
--- src/claasse/desafioData/DataTeste.java
+++ src/claasse/desafioData/DataTeste.java
@@ -3,17 +3,14 @@ package claasse.desafioData;
 public class DataTeste {
     public static void main(String[] args) {
 
-        Data data1 = new Data();
+        Data data1 = new Data(7, 8, 2001); // inicializando um novo objeto passando os valores pedidios no construtor
 
-        data1.dia = 7;
-        data1.mes = 8;
-        data1.ano = 2001;
-
+//      Chamando o construtor padrao
         Data data2 = new Data();
-
-        data2.dia = 14;
-        data2.mes = 7;
-        data2.ano = 2005;
+//        se eu nao passar nada, ele consederará os valores que eu coloquei como Default dentro do construtor
+//        data2.dia = 14;
+//        data2.mes = 7;
+//        data2.ano = 2005;
 
         System.out.println(data1.obterDataFormatada());
         System.out.println(data2.obterDataFormatada());
