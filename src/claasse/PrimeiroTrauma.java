b94c4be2c2123575fb052f7d76fe0a6f7f6f70dd
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-02-21 00:01:02 -0300

    Desafio (PrimeiroTrauma)

---
+++ src/claasse/PrimeiroTrauma.java
@@ -0,0 +1,18 @@
+package claasse;
+
+// Desafio imprimir a variavel (a) dentro do matodo main
+
+public class PrimeiroTrauma {
+    
+    int a = 3; // 
+
+    public static void main(String[] args) {
+
+        // é só instancar a classe "PrimeiroTrauma" e pegar o valor de a como referencia
+        PrimeiroTrauma primeiroTrauma = new PrimeiroTrauma();
+
+        // Desta forma consigo acessar o atributo da classe Primeiro trauma de dentro de um metodo statico "MAIN"
+        System.out.println(primeiroTrauma.a);
+
+    }
+}
