f8d6ee34af78765fdc810627a2675771ec21587a
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-02-23 20:37:28 -0300

    Exercicio Arrays

---
+++ src/arrays/ExercicioArray.java
@@ -0,0 +1,28 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ExercicioArray {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.printf("Ola, Quantas notas Deseja informar para o calculo da media?: ");
+        int quantidadeDeNotas = entrada.nextInt();
+
+        double[] notas = new double[quantidadeDeNotas];
+
+        for (int i = 0; i < quantidadeDeNotas; i++) {
+            System.out.printf("Informe a nota %d: ", i + 1);
+            double notaInformada = entrada.nextDouble();
+            notas[i] += notaInformada;
+        }
+
+        double somaNotas = 0;
+        for (double notaInformada: notas) {
+            somaNotas += notaInformada;
+        }
+        System.out.println("Media das notas: " + somaNotas / notas.length);
+    }
+}
