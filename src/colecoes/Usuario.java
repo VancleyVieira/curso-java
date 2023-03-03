6283e57be3b9f572e97bcf8657618eb1bde4f406
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-03-03 12:55:58 -0300

    HashSet() na pratica

--- src/colecoes/Usuario.java
+++ src/colecoes/Usuario.java
@@ -15,15 +15,15 @@ public class Usuario {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return Objects.equals(nome, usuario.nome);
+    public int hashCode() {
+        return Objects.hash(nome);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(nome);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario)) return false;
+        Usuario usuario = (Usuario) o;
+        return nome.equals(usuario.nome);
     }
 }
