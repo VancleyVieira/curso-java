ea8f979982acad803342d07111da4a211bb57f1f
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-03-28 19:33:57 -0300

    Resposta do desafio construtor padrao

--- src/orientacaoObjetos/heranca/desafioHeranca/Ferrai.java
+++ src/orientacaoObjetos/heranca/desafioHeranca/Ferrai.java
@@ -2,9 +2,16 @@ package orientacaoObjetos.heranca.desafioHeranca;
 
 public class Ferrai extends Carro {
 
-    void getAcelerar() {
-        super.getAcelerar();
-        super.getAcelerar();
-        super.getAcelerar();
+    Ferrai() {
+        this(315);
     }
+
+    Ferrai(int velocidadeMaxima) {
+        super(velocidadeMaxima);
+        delta = 15;
+    }
+
+//    void getAcelerar() {
+//        velocidadeAtual += 15;
+//    }
 }
--- src/orientacaoObjetos/heranca/desafioHeranca/Fusca.java
+++ src/orientacaoObjetos/heranca/desafioHeranca/Fusca.java
@@ -1,4 +1,8 @@
 package orientacaoObjetos.heranca.desafioHeranca;
 
 public class Fusca extends Carro {
+
+    Fusca() {
+        super(212);
+    }
 }
