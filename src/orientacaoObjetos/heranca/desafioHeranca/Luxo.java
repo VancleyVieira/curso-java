52c44d5def5ba696ee49ddeda682043daaa90783
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-03-30 08:02:54 -0300

    Adicionando comentarios importantes sobre interfaces

--- src/orientacaoObjetos/heranca/desafioHeranca/Luxo.java
+++ src/orientacaoObjetos/heranca/desafioHeranca/Luxo.java
@@ -4,6 +4,7 @@ public interface Luxo {
 
     // todos os metodos de uma interface são publicos e abstratos
     // quando uma classe implementa uma interface ela é obrigada a reescrever os metodos da interface
+    // Uma classe pode extender apenas um classe porém pode implementar varias interfaces
 
     public abstract void ligarAr(); // redundante
     void desligarAr();
