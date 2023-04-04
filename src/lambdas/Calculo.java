f92c92d2fc3e9163b551b687e28df7212e17cdd6
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-04-04 06:59:33 -0300

    Metodos default e static em interfaces funcionais

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,7 +1,17 @@
 package lambdas;
 
 @FunctionalInterface
-public interface Calculo { // Uma interface Funcional pode ter apenas um metodo
+public interface Calculo { // Uma interface Funcional pode ter apenas um metodo "ABSTRATO"
 
-    double executar(double a, double b);
+    double executar(double a, double b); // metodo abstrato(sem implementação) a lambda vai utilizar ele
+
+    // metodos default e staticos são uma excecão dessa regra
+
+//    default String legal() {
+//        return "Legal";
+//    }
+//
+//    static String muitoLegal() {
+//        return "Muito Legal";
+//    }
 }
