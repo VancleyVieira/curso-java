c6fb4f015ce66f08368139c026f2c903ce9dc08c
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-04-04 07:31:55 -0300

    comentarios sobre o BinaryOperator

--- src/lambdas/CalculoTeste3.java
+++ src/lambdas/CalculoTeste3.java
@@ -5,6 +5,9 @@ import java.util.function.BinaryOperator;
 public class CalculoTeste3 {
     public static void main(String[] args) {
 
+        // "BinaryOperator" função pronta da biblioteca java.util
+        // Que pega dois parametros do mesmo tipo e retorna um valor do mesmo tipo dos parametros
+
         BinaryOperator<Double> calc = (a, b) -> { return a + b; };
         System.out.println(calc.apply(2.0, 3.0));
 
