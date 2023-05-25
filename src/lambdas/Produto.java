b0453efbf3e3127aa7cec748665bf670b5a41eba
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-05-25 15:34:46 -0300

    Interface funcional do java PREDICATE

---
+++ src/lambdas/Produto.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+// Toda classe em java extende de Object
+public class Produto extends Object {
+
+    final String nome;
+    final double preco;
+    final double desconto;
+
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+
+
+}
