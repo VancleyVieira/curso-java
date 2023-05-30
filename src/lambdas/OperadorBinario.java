e450fb6f01fdfe9b0c946654c0db1a92f515a1a4
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-05-30 07:09:32 -0300

    Operador Binario

---
+++ src/lambdas/OperadorBinario.java
@@ -0,0 +1,25 @@
+package lambdas;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class OperadorBinario {
+    public static void main(String[] args) {
+
+        BinaryOperator<Double> media = (n1, n2) -> (n1 + n2) /2;
+        System.out.println(media.apply(9.8, 5.7));
+
+        BiFunction<Double, Double, String> resultado = (n1 , n2) -> {
+            double notaFinal = (n1 + n2) / 2;
+            return notaFinal >= 7 ? "Aprovado" : "Reprovado";
+        };
+        System.out.println(resultado.apply(9.7,3.1));
+
+        Function<Double, String> conceito =
+                nota -> nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(media.andThen(conceito).apply(7.0,8.3));
+
+    }
+}
